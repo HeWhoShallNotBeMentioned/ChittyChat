e738a0522288be615c0838688b33fe11035e41a8
Chris Underwood <cunderwoodmn@gmail.com>
2017-04-16 22:31:32 -0700

    add error handling if there is no proper file path.

--- index.js
+++ index.js
@@ -2,12 +2,22 @@ const http = require('http');
 const fs = require('fs');
 const extract = require('./extract');
 
+var handleError = function (err, res) {
+  res.writeHead(404);
+  res.end();
+};
+
 var server = http.createServer(function (req, res) {
   console.log('Responding to a request');
 
   var filePath = extract(req.url);
   fs.readFile(filePath, function(err, data){
-    res.end(data);
+    if (err) {
+      handleError(err, res);
+      return;
+    } else {
+        res.end(data);
+      }
   });
 });
 server.listen(3000);
