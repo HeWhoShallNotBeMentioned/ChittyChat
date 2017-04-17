1f4d1b2d0dd5ef9323c21428f50a867497f2fd36
Chris Underwood <cunderwoodmn@gmail.com>
2017-04-16 22:19:02 -0700

    move the code that extracts the file path from index.js into its own file and connect the files.

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
 const http = require('http');
 const fs = require('fs');
-const path = require('path');
+const extract = require('./extract');
 
 var server = http.createServer(function (req, res) {
   console.log('Responding to a request');
-  var url = req.url;
 
-  var fileName = 'index.html';
-  if (url.length > 1 ) {
-    fileName = url.substring(1);
-  }
-  console.log(fileName);
-  var filePath = path.resolve(__dirname,'app', fileName);
+  var filePath = extract(req.url);
   fs.readFile(filePath, function(err, data){
     res.end(data);
   });
