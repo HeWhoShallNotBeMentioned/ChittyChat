3c053f630de0ccf2c3b23b54d23bdcf5e8fa33ed
Chris Underwood <cunderwoodmn@gmail.com>
2017-05-01 21:51:08 -0700

    created an array to store all messages, a forEach function for every new person to be sent all messages, push to send messages to the array, and 2nd for each for ws to send a new message to all users

--- websockets-server.js
+++ websockets-server.js
@@ -5,14 +5,23 @@ var ws = new WebSocketServer({
   port: port
 });
 
+let messages = [];
+
 console.log('websockets server started');
 
 ws.on('connection', function (socket){
   console.log('client connection established');
 
+  messages.forEach(function (msg){
+    socket.send(msg);
+  });
 
   socket.on('message', function(data){
     console.log('message received: ' + data);
-    socket.send(data);
+    messages.push(data);
+    ws.clients.forEach(function (clientSocket){
+      clientSocket.send(data);
+    });
+
   });
 });
